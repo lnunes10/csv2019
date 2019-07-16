b5aef59ff2556c4b8eaa596b27ff121feb7cd019
lnunes10 <ln64@njit.edu>
2019-07-16 18:38:09 -0400

    added php file

--- index.php
+++ index.php
@@ -1,78 +1,68 @@
 <?php
 main::start("SacramentocrimeJanuary2006.csv");
-class main
-{
-    static public function start ($filename){
-        $records=csv::getRecords($filename);
-        html:: generateTable($records);
-
+class main  {
+    static public function start($filename) {
+        $records = csv::getRecords($filename);
+        $table = html::generateTable($records);
     }
-}
-
 }
 class html {
-    public static function generateTable($records){
-        foreach ($records as $record){
-            $fields=get_object_vars($record);
-            print_r($fields);
-
-        }
-    }
-class csv{
-    static public function getRecords($filename){
-        $file =fopen($filename,"r");
-        $fieldNames=Array();
-        $count=0;
-
-        while (!feof($file)) {
-            $record = fgetcsv($file);
-            if ($count == 0)
-            {
-                $fieldNames = $record;
-            }
-            else{
-                $records[] = recordFactory::create($fieldNames,$record);
-
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
             }
             $count++;
-
         }
-
-
+    }
+}
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
         fclose($file);
         return $records;
-
-
     }
-
 }
-class record{
-    public function __construct(Array $fieldNames =null,$values=null)
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
     {
-
-        $record=array_combine($fieldNames,$values);
-        $record=(object) $record;
-        foreach ($record as $property=>$value){
-            $this->createProperty($property,$value);
-
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
         }
-
-
     }
-    public function returnArray(){
-        $array =(array) $this;
+    public function returnArray() {
+        $array = (array) $this;
         return $array;
     }
-    public function createProperty($name='cdatetime',$value='address'){
-        $this->{$name}=$value;
-
+    public function createProperty($name = 'first', $value = 'keith') {
+        $this->{$name} = $value;
     }
 }
-class recordFactory{
-    public static function create (Array $fieldNames =null, Array $values=null){
-
-        $record=new record ($fieldNames,$values);
-
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
         return $record;
     }
 }
